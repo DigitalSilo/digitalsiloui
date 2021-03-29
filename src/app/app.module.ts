e6208e0bdf19c0c3da387726f27f461cc5aa2ee7
Arash Sabet <arash@umplify.com>
2021-03-28 20:21:53 -0400

    Cleaned up the app component to start integrating the grains status component #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GrainsStatusComponent } from './grains-status/grains-status.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +13,8 @@ import { GrainsStatusComponent } from './grains-status/grains-status.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
