4f07164476d5f91cd4d4df9919f4802a25fb6982
Arash Sabet <arash@umplify.com>
2021-04-22 20:37:22 -0400

    Added isSystemResponse boolean flag to distinguish between system grains and developers' #11

--- src/app/models/response.ts
+++ src/app/models/response.ts
@@ -1,5 +1,7 @@
 export class Response {
     resultCode: number = 0;
     message: string = '';
+    isSystemResponse: boolean = false;
     details: Array<any> = new Array<any>();
+    responseTimeUTC: Date | undefined;
 }
