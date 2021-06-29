820a1fdeeee32b02380a23b52e97a3bf733a22d8
Arash Sabet <arash@umplify.com>
2021-06-28 20:41:17 -0400

    Changed term Grain Fabric to Digital silo #40
    The CI/CD pipeline updated accordingly too.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Grainfabricadminui
+# Digital Silo UI
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'grainfabricadminui'`, () => {
+  it(`should have as title 'digitalsiloui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('grainfabricadminui');
+    expect(app.title).toEqual('digitalsiloui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('grainfabricadminui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('digitalsiloui app is running!');
   });
 });
